653b0147f3336a09a378266e13b27ca973142a6b
Ade11edA <150388135+Ade11edA@users.noreply.github.com>
2024-05-18 19:05:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.querySelector(".d-flex").addEventListener("submit", function(e){
     e.preventDefault();
     let search = document.querySelector('input').value;
     console.log(search);
-    if(search == "duck"){
+    if(search.toLowerCase() == "duck"){
         location.href = "duck.html"
     }
 });
